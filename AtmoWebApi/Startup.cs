02fcf7de88a9ecf5b590f9e7e545c1274494e5a1
elektromntr <bartosztadeuszwojcik@gmail.com>
2020-05-04 19:01:02 +0200

    Core 3.1 upgrade finish

--- AtmoWebApi/Startup.cs
+++ AtmoWebApi/Startup.cs
@@ -28,6 +28,7 @@ namespace AtmoWebApi
 				Configuration["Data:AtmoWebApi:ConnectionString"]));
 			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 			services.AddTransient<ITicketService, TicketService>();
+			services.AddControllers();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,8 @@ namespace AtmoWebApi
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-
+			app.UseRouting();
+			app.UseEndpoints(endpoints => endpoints.MapControllers());
 			app.UseHttpsRedirection();
 			SeedData.DoSeed(app);
 		}
